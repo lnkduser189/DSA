be49ab528754fa27ec6d1e0ecc0b96e8a2e63b3a
Vaishnavi Rushikesh Matade <vaishnavi@Vaishnavis-MacBook-Air.local>
2026-08-03 23:18:03 +0530

    Add Longest Repeating Character Replacement solution

---
+++ Strings/Longest_Repeating_Character_Replacement.cpp
@@ -0,0 +1,35 @@
+
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+        int freq[26] = {0};
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s[right] - 'A']++;
+
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) {
+
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
